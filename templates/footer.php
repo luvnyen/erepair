5fe05d3ba55ba6ada0d06ff0509109b831d9b28b
Calvert Tanudihardjo <calvert.tanudihardjo@gmail.com>
2023-05-10 22:07:50 +0700

    feature: navbar, footer, and home page

---
+++ templates/footer.php
@@ -0,0 +1,79 @@
+<link rel="stylesheet" href="./style/templates/footer.css">
+
+<div class="footer__container">
+        <div class="row">
+            <div class="col-md-2">
+                <ul class=".footer__link--vertical">
+                    <li>
+                        <a href=" #">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Join Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <ul class=".footer__link--vertical">
+                    <li>
+                        <a href="#">Help Center</a>
+                    </li>
+                    <li>
+                        <a href="#">Feedback</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer__section--right col-md-8">
+                <img src="./assets/templates/erepair-logo.svg" width="200" alt="ERepair Logo" loading="lazy">
+                <br>
+                <div class="footer__social mt-3">
+                    <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
+                        <img src="./assets/templates/social-google.svg" width="40" alt="Google+" loading="lazy">
+                    </a>
+                    <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
+                        <img src="./assets/templates/social-instagram.svg" class="ml-2" width="40" alt="Instagram" loading="lazy">
+                    </a>
+                    <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
+                        <img src="./assets/templates/social-facebook.svg" class="ml-2" width="40" alt="Facebook" loading="lazy">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-6">
+                <ul class="footer__link--horizontal">
+                    <li>
+                        <a href="#">CONTACT US</a>
+                    </li>
+                    <li class="ml-5">
+                        <a href="#">PRIVACY POLICY</a>
+                    </li>
+                    <li class="ml-5">
+                        <a href="#">TERMS OF USE</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer__section--right footer__copyright col-md-6">
+                <ul>
+                    <li>&copy; <?= date("Y") ?> ERepair Inc.</li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- Optional JavaScript; choose one of the two! -->
+
+        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+        <!-- Option 2: SeparatePopper and Bootstrap JS -->
+        <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- import css from style/header.css -->
+    <link rel="stylesheet" href="./style/templates/header.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <title>ERepair</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="#">
+            <img src="./assets/templates/erepair-logo-left.svg" width="35" height="40" alt="ERepair Logo" loading="lazy">
+            <img src="./assets/templates/erepair-logo.svg" width="130" height="50" alt="ERepair Logo" loading="lazy">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto mr-5">
+                <li class="nav-item active">
+                    <a href="#">Sign In</a>
+                </li>
+
+            </ul>
+            <form class="form-inline my-2 my-lg-0" action="#" method="post">
+                <div class="search__container">
+                    <input class="search__input form-control" type="search" placeholder="Search" aria-label="Search" required>
+                    <button class="search__button btn my-2 my-sm-0" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </nav>
